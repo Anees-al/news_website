f28fdf92e858c1e1e82561666eef43c9860debf1
Anees A L <aneesal874@gmail.com>
2026-02-02 11:11:11 +0530

    add technology section

--- backend/controllers/newController.js
+++ backend/controllers/newController.js
@@ -70,3 +70,26 @@ export const getbusiness=async(req,res)=>{
         return res.status(500).json({message:error.message,success:false})
     }
 }
+
+
+
+export const gettechnology=async(req,res)=>{
+    try {
+         const { country="us" , category } = req.query;
+         const responce=await axios.get(
+            "https://newsapi.org/v2/top-headlines?category=technology",
+            {
+                params: {
+          country,
+          category,
+          apiKey: process.env.API_KEY,
+        },
+            }
+         )
+
+
+         return res.status(200).json({success:true,message:'successfully fetched',articles:responce.data.articles})
+    } catch (error) {
+        return res.status(500).json({message:error.message,success:false})
+    }
+}
--- backend/routes/newsRoute.js
+++ backend/routes/newsRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getbusiness, getSports, getTopHeadline } from '../controllers/newController.js';
+import { getbusiness, getSports, gettechnology, getTopHeadline } from '../controllers/newController.js';
 
 const router=express.Router();
 
 router.get('/top-headlines',getTopHeadline);
 router.get('/sports',getSports);
 router.get('/business',getbusiness);
+router.get('/technology',gettechnology);
 
 export default router;
